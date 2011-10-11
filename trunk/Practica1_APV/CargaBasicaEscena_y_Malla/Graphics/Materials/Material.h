e290a00a543c09a9616ce95b46bddbc0e54e07e9
weezerin@gmail.com <weezerin@gmail.com@574b8e12-f593-b39b-9f9b-2334fb5dfc15>
2011-10-11 00:20:27 +0000

    11/10/2011 Juan Manuel Ramón Vigo <weezerin@gmail.com>    Practica1_APV/*:Added project with part of the functionality (implemented until 2.5 section)

---
+++ trunk/Practica1_APV/CargaBasicaEscena_y_Malla/Graphics/Materials/Material.h
@@ -0,0 +1,41 @@
+// Class to obtain data of resource files
+
+#ifndef Material_H
+#define Material_H
+
+#include <vector>
+#include "..\..\Utility\Resource.h"
+#include "..\..\Utility\ResourceHandle.h"
+#include <sstream>
+#include <aiMaterial.h>										// C++ importer interface
+#include "MaterialData.h"
+
+// Struct to handle texture and name 
+struct cTextureData
+{
+	std::string macShaderTextureID;
+	cResourceHandle mTexture;
+};
+
+/*******************/
+
+class cMaterial : public cResource {
+	public:
+		cMaterial() { mbLoaded = false; }
+		virtual bool Init( const std::string &lacNameID, void * lpMemoryData, int liDataType );
+		virtual void Deinit();
+		virtual bool IsLoaded() { return mbLoaded; }
+		//void SetMaterial();
+		void PrepareRender();
+		bool SetFirstPass();
+		bool SetNextPass();
+	private:
+		void ReadAllTextures(aiMaterial * lpAiMaterial, cMaterialData * lpMaterialData);
+		std::string macFile;
+		std::vector<cTextureData> maTextureData;
+		//cResourceHandle mDiffuseTexture;
+		cResourceHandle mEffect;
+		bool mbLoaded;
+};
+
+#endif
