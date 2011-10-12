daba1bf76dc144e4dfe4a464d79fbcd00950fb7a
weezerin@gmail.com <weezerin@gmail.com@574b8e12-f593-b39b-9f9b-2334fb5dfc15>
2011-10-12 15:07:40 +0000

    12/10/2011 Juan Manuel Ramón Vigo <weezerin@gmail.com>    Practica1/*:Added project with all functionality

---
+++ trunk/Practica1/CargaBasicaEscena_y_Malla/Gameplay/Object/Object.cpp
@@ -0,0 +1,44 @@
+#include "Object.h"
+#include "..\..\Graphics\GraphicManager.h"
+#include "..\..\Graphics\Materials\Material.h"
+#include "..\..\Graphics\Meshes\Mesh.h"
+#include "..\Scene\Scene.h"
+
+// Reset name, world matrix, and vectors
+void cObject::Init()
+{
+	macName = "";
+	mWorldMatrix.LoadIdentity();
+	mMeshHandles.resize(0);
+	mMaterialHandles.resize(0);
+}
+
+void cObject::AddMesh( cResourceHandle lMeshHandle, cResourceHandle lMaterialHandle ) 
+{
+	mMeshHandles.push_back( lMeshHandle );
+	mMaterialHandles.push_back( lMaterialHandle );
+}
+
+void cObject::Render()
+{
+	// Set World Matrix
+	cGraphicManager::Get().SetWorldMatrix(mWorldMatrix);
+	for (unsigned luiIndex = 0; luiIndex < mMeshHandles.size(); ++luiIndex)
+	{
+		// Set the material
+		cMaterial* lpMaterial = ( cMaterial* ) mMaterialHandles[luiIndex].GetResource();
+		cMesh *lpMesh = (cMesh *)mMeshHandles[luiIndex].GetResource();
+
+		// Prepare all the parameters for the render
+		lpMaterial->PrepareRender();
+		// Set the fist pass
+		bool lbContinue = lpMaterial->SetFirstPass();
+		while (lbContinue) {
+			// Render Mesh
+			lpMesh->RenderMesh();
+			// Prepare the next pass
+			lbContinue = lpMaterial->SetNextPass();
+		}
+	}
+}
+
