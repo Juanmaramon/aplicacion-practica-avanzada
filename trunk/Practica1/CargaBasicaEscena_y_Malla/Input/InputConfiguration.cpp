bbb5878c4149b8ffbb827579321026780e58cae0
weezerin@gmail.com <weezerin@gmail.com@574b8e12-f593-b39b-9f9b-2334fb5dfc15>
2011-10-13 22:04:04 +0000

    14/10/2011 Juan Manuel Ramón Vigo <weezerin@gmail.com>    Practica1/*:Added project with all functionality. I forgot tinyXML functionality.

--- trunk/Practica1/CargaBasicaEscena_y_Malla/Input/InputConfiguration.cpp
+++ trunk/Practica1/CargaBasicaEscena_y_Malla/Input/InputConfiguration.cpp
@@ -7,7 +7,8 @@
 //El primer parámetro de cada terna es del enumerado "eInputActions" del fichero InputConfiguration.h,
 // y el segundo parámetro es del enumerado "eDevices" de InputManager.h.
 tActionMapping kaActionMapping[] = {
-	{ eIA_CloseApplication, eKeyboard, OIS::KC_ESCAPE }, //Esta fila indica que la acción salir con el teclado está asociada a la tecla escape.	                                                     	
+	{ eIA_CloseApplication, eKeyboard, OIS::KC_ESCAPE }, //Esta fila indica que la acción salir con el teclado está asociada a la tecla escape.	 
+	{ eIA_ReloadEffectManager, eKeyboard, OIS::KC_R }, // This row associate R keyboard key with the reload action
 	{ eIA_CloseApplication, ePad, ePad_Button1 }, //Esta fila indica que la acción salir con el JoyStick/GamePad está asociada al botón 1.	                                                     	 
 	{ eIA_CloseApplication, eMouse, eMouse_ButtonMiddle }, //Esta fila indica que la acción salir con el mouse está asociada al botón central.	                                                     	 
     { -1, -1, -1}  // End of the table (Marca el final de la tabla)
